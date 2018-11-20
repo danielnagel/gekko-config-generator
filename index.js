755206b658794accbe8d04f4e29151168325463e
Daniel <danielsenpai95@gmail.com>
2018-11-20 14:56:52 +0100

    added comments to the source code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+/**
+ * Main script of the project 'gekko config generator'.
+ */
+
+// import packages
 const program = require("commander");
 const tomlToJson = require("./lib/toml-to-json");
 const configGenerator = require("./lib/config-generator");
-const Logger = require('./lib/logger');
+const Logger = require("./lib/logger");
 
 // constant variables
 const pathToToml = "config/strategies";
@@ -14,13 +19,17 @@ let currency = "USDT";
 let asset = "BTC";
 let logLevel = 3;
 
+// parse command line arguments
 program
   .version("0.3.0")
   .option("-p, --path <value>", "path to gekko")
   .option("-e, --exchange <value>", "specify the exchange")
   .option("-c, --currency <value>", "specify the currency")
   .option("-a, --asset <value>", "specify the asset")
-  .option("-l, --loglevel <value>", "set the log level, info/all(1), warning(2), error(3), nothing(4)")
+  .option(
+    "-l, --loglevel <value>",
+    "set the log level, info/all(1), warning(2), error(3), nothing(4)"
+  )
   .parse(process.argv);
 
 if (typeof program.path !== "undefined") path = program.path;
@@ -32,10 +41,12 @@ if (typeof program.loglevel !== "undefined") logLevel = program.loglevel;
 if (path.charAt(path.length - 1) !== "/") path += "/";
 
 const options = `{"exchange": "${exchange}", "currency": "${currency}", "asset": "${asset}"}`;
-const log = new Logger(logLevel);
 
+// initialize logger
+const log = new Logger(logLevel);
 log.welcome();
 
+// main function
 tomlToJson(`${path}${pathToToml}`, log).forEach(json => {
   configGenerator(json, options, `${path}${pathToConfigDir}`, log);
 });
